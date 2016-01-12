0df70ebf3120df87034f8851985991f06e6f4654
a169674 <a169674@externos.bankia.com>
2016-01-12 12:01:33 +0100

    Support for tvOS

---
+++ SVProgressHUDTV/SVProgressHUDTV.h
@@ -0,0 +1,19 @@
+//
+//  SVProgressHUDTV.h
+//  SVProgressHUDTV
+//
+//  Created by Fidel on 12/1/16.
+//  Copyright © 2016 EmbeddedSources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVProgressHUDTV.
+FOUNDATION_EXPORT double SVProgressHUDTVVersionNumber;
+
+//! Project version string for SVProgressHUDTV.
+FOUNDATION_EXPORT const unsigned char SVProgressHUDTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVProgressHUDTV/PublicHeader.h>
+
+
